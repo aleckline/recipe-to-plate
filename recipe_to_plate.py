d3b540b1d1518354374890ce7c6b9b93fc2b4b6e
Alec Kline <git config --global user.email 287547516+aleckline@users.noreply.github.com>
2026-06-07 09:43:25 -0400

    Use nested dictionary and round quantities

--- recipe_to_plate.py
+++ recipe_to_plate.py
@@ -1,22 +1,29 @@
+# Recipe data
 aglio = {
-	'pasta, g': '90',
-	'olive oil, g':'8',
-	'garlic, g':'10',
-	'red pepper, tsp':'.2',
-	'chicken, oz': '5',
-	'lemon, ea':'.4',
-	'pepper, tsp':'.4',
-	'burrata, g': '11',
-	'parsely, bunch':'0.1',
+	'pasta': {'qty': 90, 'unit':'grams'},
+	'olive oil': {'qty': 8, 'unit':'grams'},
+	'garlic': {'qty': 10, 'unit':'grams'},
+	'red pepper': {'qty': 0.2, 'unit':'tsp'},
+	'chicken': {'qty': 5, 'unit':'oz'},
+	'lemon': {'qty': 0.4, 'unit':'ea'},
+	'pepper': {'qty': 0.4, 'unit':'tsp'},
+	'burrata': {'qty': 11, 'unit':'grams'},
+	'parsely': {'qty': 0.1, 'unit':'bunch'}
 					}
 
-
+# Receive the meal plan
 message = input('What are you eating this week? ')
 meals = input('How many meals are you eating this week? ')
 
+
+# Create the shopping list
 shopping_list = {}
-for ingredient, quantity in aglio.items():
-	ingredient_total = float(quantity)*int(meals)
-	shopping_list[ingredient] = ingredient_total
+for ingredient, info in aglio.items():
+	ingredient_quantity = round(float(info['qty'])*int(meals), 2)
+	total_unit = f"{ingredient_quantity} {info['unit']}"
+	shopping_list[ingredient] = total_unit
+
+
+# Print a formmated shopping list
 for key, value in shopping_list.items():
 	print(key.capitalize() + ': ' + str(value))
