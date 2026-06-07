3619e05e95eaddf84d740c5cc9db762544898d0d
Alec Kline <git config --global user.email 287547516+aleckline@users.noreply.github.com>
2026-06-07 12:28:44 -0400

    Add recipe book and multiple recipe capability

--- recipe_to_plate.py
+++ recipe_to_plate.py
@@ -10,18 +10,48 @@ aglio = {
 	'burrata': {'qty': 11, 'unit':'grams'},
 	'parsely': {'qty': 0.1, 'unit':'bunch'}
 					}
+ragu = {
+	'pasta': {'qty': 90, 'unit':'grams'},
+	'ground turkey': {'qty': 5, 'unit':'oz'},
+	'bacon': {'qty': 10, 'unit':'grams'},
+	'tomato sauce': {'qty': 8, 'unit':'oz'},
+					}
+
+recipe_book = {'aglio': aglio, 'ragu': ragu}
+menu = []
+for recipe in recipe_book.keys():
+	menu.append(recipe.title())
+print('Menu: ')
+print(str(menu) + '\n')
 
 # Receive the meal plan
-message = input('What are you eating this week? ')
-meals = input('How many meals are you eating this week? ')
+meal_plan = {}
+gathering_info = True
+while gathering_info:
+	recipe = input('What recipe are we calculating? ')
+	recipe_qty = input('How many meals of ' + recipe + '? ')
+	meal_plan[recipe] = recipe_qty
+	another = input('Are we adding another recipe? (yes/no) ')
+	print('\n')
+	if another == 'no':
+		gathering_info = False
 
+print('Meal plan: ')
+for meal, qty in meal_plan.items():
+	print(meal.title() + ': ' + str(qty))
 
+print('\nShopping list:')
 # Create the shopping list
 shopping_list = {}
-for ingredient, info in aglio.items():
-	ingredient_quantity = round(float(info['qty'])*int(meals), 2)
-	total_unit = f"{ingredient_quantity} {info['unit']}"
-	shopping_list[ingredient] = total_unit
+for req_recipe, req_qty in meal_plan.items():
+	if req_recipe in recipe_book:
+		recipe_data = recipe_book[req_recipe]
+		for ingredient, info in recipe_data.items():
+			ingredient_quantity = round(float(info['qty'])*int(req_qty), 2)
+			total_unit = f"{ingredient_quantity} {info['unit']}"
+			shopping_list[ingredient] = total_unit
+	else:
+		print(req_recipe + ' N/A')
 
 
 # Print a formmated shopping list
